f96a229ede6b11afddead925b808284e1f460258
AtomIntegerGood <50480516+AtomIntegerGood@users.noreply.github.com>
2019-05-10 16:25:00 +0800

    Update Return999.java

--- Return999.java
+++ Return999.java
@@ -1,17 +1,18 @@
 import java.util.Scanner;
 /*
-Import plz;
+import plz!;
 */
 class Return999{
-protected int a(int a){
-    if(a!=999){
-    a=999;
+    protected int a(int a){
+        if(a!=999){
+            a=999;
+        }
+        return a;
     }
-    return a;
-};
-public static void main(String args[]){
-      Return999 c=new Return999();
-      Scanner s=new Scanner(System.in);
-      System.out.println(c.a(1));
-      }
+    public static void main(String args[]){
+        Return999 c=new Return999();
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        System.out.println(c.a(a));
+    }
 }
